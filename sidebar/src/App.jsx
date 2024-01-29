b5859e62a0eb60b5d54eb6c11b9320b2d9a55070
MLDODSON <paola@dominicansoul.com>
2024-01-28 20:12:52 -0600

    Branch 1
    Branch 1 - Coding Addict sidebar project initial setup.

---
+++ sidebar/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>Coding Addict Sidebar</h1>
+  )
+};
+
+export default App;
---
+++ sidebar/src/components/Modal.jsx
@@ -0,0 +1,7 @@
+const Modal = () => {
+    return (
+      <h1>Modal</h1>
+    )
+  };
+  
+  export default Modal;
